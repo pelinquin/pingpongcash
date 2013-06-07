ca6e6d7103eb6bf67a36208e3aa00d900c96a615
pelinquin <pelinquin@gmail.com>
2013-06-07 20:36:39 +0200

    clean

--- server.py
+++ server.py
@@ -1161,7 +1161,6 @@ def pdf_digital_check(dusr, dtrx, dags, gr):
         (465, 0, 1, 4, '.8 .7 .9', __digest__.decode('ascii')), 
         ]
     url = (urllib.parse.quote('http://pingpongcash.net/%s/%s' % (msgraw, sig)), 'http://pingpongcash.net/%s' % src)
-    url = (urllib.parse.quote('localhost/%s/%s' % (msgraw, sig)), 'localhost/%s' % src)
     qr1, qr2 = QRCode(data=url[0]), QRCode(data=url[1])
     a = updf(496, 227) # 175x80
     return a.gen(page, pagec, qr1.pdf(17, 135, 2, True), qr2.pdf(424, 145, 2), url)
